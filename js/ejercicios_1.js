2592938b8ca4ad155ec146986ce7f77c9cc555bf
keas1126 <keas11262000@gmail.com>
2021-11-19 01:19:19 -0500

    version2

---
+++ js/ejercicios_1.js
@@ -0,0 +1,32 @@
+// Se requiere recibir 5 notas  de un estudiante y obtener el promedio de las notas
+var nota1=0
+var nota2=0
+var nota3=0
+var nota4=0
+var nota5=0
+var notaFinal=(nota1+nota2+nota3+nota4+nota5)/5
+console.log(notaFinal)
+
+// Se requiere validar si una persona ingresa el nombre, apellido y la edad e indicar si es mayor de 18 años
+var Nombre="Kevin"
+var Apellido="Arroyave"
+var Edad=21
+
+if (Nombre!="")
+console.log("no ha ingresado el nombre")
+
+if (Apellido!="")
+
+console.log("no ha ingresado apellido")
+
+if (Edad>=18)
+console.log(" es mayor de edad");
+else
+console.log("Es menor de edad");
+
+// tarea para mañana :
+
+// validar que el nombre solo sean letras
+// validar que la edad solo sean numeros
+// validar que la fecha de nacimiento reciba letras y numeros ejemplo
+// 02/05/2021.
